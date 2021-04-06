9fc1de1a8e7c128269f6c2445c1260ac9313bd33
Manuel Salvatori <manuel.salvatori@gmail.com>
2021-04-06 09:37:50 +0100

    GitIgnore update added exclusion override for folder which must be included

---
+++ MB.Business.User/GetUserByIdQuery.cs
@@ -0,0 +1,23 @@
+﻿using Minded.CommandQuery.Query;
+using Minded.Common.Query.Trait;
+using Minded.Log;
+
+namespace MB.Business.User
+{
+    public class GetUserByIdQuery : IQuery<Data.Entities.User>, ICanExpand
+    {
+        public int Id { get; }
+
+        public GetUserByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public LogInfo ToLog()
+        {
+            return new LogInfo();
+        }
+
+        public string[] Expand { get; set; }
+    }
+}
---
+++ MB.Business.User/GetUserByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MB.Data.Access;
+using MB.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Minded.CommandQuery.Query;
+
+namespace MB.Business.User
+{
+    public class GetUserByIdQueryHandler : IQueryHandler<GetUserByIdQuery, Data.Entities.User>
+    {
+        private readonly IMindedBankingContext _context;
+        private readonly ILogger<IQueryHandler<GetUserByIdQuery, Data.Entities.User>> _logger;
+
+        public GetUserByIdQueryHandler(IMindedBankingContext context, ILogger<IQueryHandler<GetUserByIdQuery, Data.Entities.User>> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+   
+        public async Task<Data.Entities.User> HandleAsync(GetUserByIdQuery query)
+        {
+            return await query.ApplyTo(_context.Users.Where(u => u.ID == query.Id).AsQueryable()).FirstOrDefaultAsync();
+        }
+    } 
+}
